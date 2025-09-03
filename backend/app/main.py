8e87abb313e3882b9b7449e7de98e2297b1df72f
Kaicheng158 <kaicheng.liang@my.jcu.edu.au>
2025-09-03 21:25:33 +1000

    Update main include workout router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,8 @@
-# app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.settings import settings
 from app.api.auth import router as auth_router
-from api.workout import router as workout_router 
+from app.api.workout import router as workout_router
 
 app = FastAPI()
 
@@ -20,5 +19,4 @@ def root():
     return {"status": "ok"}
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
-
-app.include_router(workout_router, prefix="/workout_sessions", tags=["workout"])
+app.include_router(workout_router, prefix="/workouts", tags=["workouts"])
